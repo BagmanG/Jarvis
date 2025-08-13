498dcaef4e0a0739ee1ea158bcd92d84a89e3cce
BagmanG <zagr.gadel@gmail.com>
2025-08-14 00:28:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 require_once 'Core/EnvReader.php';
 
+EnvReader::load();
 define("BOT_TOKEN", EnvReader::get('BOT_TOKEN'));
 
 echo "Hello World Test :".BOT_TOKEN;
