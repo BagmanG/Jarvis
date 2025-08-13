721b212b2f6253272a6145c5bb3c8194486233b4
BagmanG <zagr.gadel@gmail.com>
2025-08-13 23:50:06 +0300

    Show errors

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 require_once 'Core/EnvReader.php';
 
 define("BOT_TOKEN", EnvReader::get('BOT_TOKEN'));
