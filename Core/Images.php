cfa8b92a3b91f2c153344f360f2b531cab03168f
BagmanG <zagr.gadel@gmail.com>
2025-08-14 23:56:12 +0300

    Fix

--- Core/Images.php
+++ Core/Images.php
@@ -1,6 +1,6 @@
 <?php
 
 class Images {
-    public static $host = "https://bagmanov.com/projects/jarvis/assets/images/";
-    public static $start = $host."start.jpg";
+    const HOST = "https://bagmanov.com/projects/jarvis/assets/images/";
+    public static $start = self::HOST . "start.jpg";
 }
