edfbc7551c9cc878a2419958c77217eeddff3abf
BagmanG <zagr.gadel@gmail.com>
2025-08-19 22:44:39 +0300

    Support System

--- Core/Init.php
+++ Core/Init.php
@@ -7,4 +7,5 @@ define("BOT_TOKEN", EnvReader::get('BOT_TOKEN'));
 define("AI_TOKEN", EnvReader::get('AI_TOKEN'));
 define("DB_PASSWORD", EnvReader::get('DB_PASSWORD'));
 define("DB_NAME", EnvReader::get('DB_NAME'));
+define("SUPPORT_CHAT_ID", EnvReader::get('SUPPORT_CHAT_ID'));
 ?>
