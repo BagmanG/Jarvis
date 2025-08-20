67b509507913dcd922c4970a8a8ef71673e5e9f0
BagmanG <zagr.gadel@gmail.com>
2025-08-20 22:58:53 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', getenv('DB_NAME'));
-define('DB_PASS', getenv('DB_PASSWORD'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_NAME', getenv('DB_NAME'));
 define('BOT_TOKEN', getenv('BOT_TOKEN'));
 function getEnvData($key, $default = null) {
