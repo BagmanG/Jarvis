c91c57f3d04e7f87beffc68514f39787450f900b
BagmanG <zagr.gadel@gmail.com>
2025-08-24 18:55:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,12 @@ require_once 'Core/Events.php';
 
 // Функция для логирования ошибок
 function logError($message) {
-    error_log(date('Y-m-d H:i:s') . ' - ' . $message . PHP_EOL, 3, 'error.log');
+    $file = __DIR__ . '/tg_errors.log';
+// Логируем выполнение
+file_put_contents($file, 
+    date('Y-m-d H:i:s') . " - " . $message . "\n", 
+    FILE_APPEND
+);
 }
 
 $content = file_get_contents("php://input");
