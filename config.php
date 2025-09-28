20065160f7aa32c0ff176335740b19e8a5a578e7
BagmanG <zagr.gadel@gmail.com>
2025-09-28 16:05:20 +0300

    фикс

--- config.php
+++ config.php
@@ -1,17 +1,16 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', getEnvData('DB_NAME'));
-define('DB_PASSWORD', getEnvData('DB_PASSWORD'));
-define('DB_NAME', getEnvData('DB_NAME'));
-define('BOT_TOKEN', getEnvData('BOT_TOKEN'));
-function getEnvData($key, $default = null) {
-    $envFile = __DIR__ . '/.env';
-    
-    if (!file_exists($envFile)) {
-        return $default;
+if (!function_exists('getEnvData')) {
+    function getEnvData($key, $default = null) {
+        $envFile = __DIR__ . '/.env';
+        if (!file_exists($envFile)) {
+            return $default;
+        }
+        $envData = parse_ini_file($envFile);
+        return $envData[$key] ?? $default;
     }
-    
-    $envData = parse_ini_file($envFile);
-    
-    return $envData[$key] ?? $default;
 }
+if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
+if (!defined('DB_USER')) define('DB_USER', getEnvData('DB_NAME'));
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', getEnvData('DB_PASSWORD'));
+if (!defined('DB_NAME')) define('DB_NAME', getEnvData('DB_NAME'));
+if (!defined('BOT_TOKEN')) define('BOT_TOKEN', getEnvData('BOT_TOKEN'));
