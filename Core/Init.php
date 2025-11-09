1e0f5a366217b07be59a63355868b5c03e00d9ad
BagmanG <zagr.gadel@gmail.com>
2025-11-09 15:58:29 +0300

    test dev

--- Core/Init.php
+++ Core/Init.php
@@ -10,5 +10,5 @@ define("AI_TOKEN", EnvReader::get('AI_TOKEN'));
 define("DB_PASSWORD", EnvReader::get('DB_PASSWORD'));
 define("DB_NAME", EnvReader::get('DB_NAME'));
 define("SUPPORT_CHAT_ID", EnvReader::get('SUPPORT_CHAT_ID'));
-define("IS_DEV", false); // Поставьте false для production
+define("IS_DEV", true); // Поставьте false для production
 ?>
